e4852ab40b89f7758935dec4f87e946903cc7dd3
pad0220 <pad@shipbuilders.ru>
2025-01-31 11:16:51 +0300

    change plugin loader

--- include/PluginLoader/PluginLoader.h
+++ include/PluginLoader/PluginLoader.h
@@ -7,10 +7,36 @@
 #include <QPluginLoader>
 #include <QDebug>
 #include "BaseNaviWidget.h"
+#include <QMetaEnum>
+#define DEFAULT_DATA_DIR QDir::toNativeSeparators(QDir::homePath()) + QDir::separator() +"Lazuli" + QDir::separator() 
+
+#define PLAGIN_DATA_DIR DEFAULT_DATA_DIR + "Plugins"
 
 class PluginLoader : public QObject {
     Q_OBJECT
 public:
+    enum class ErrorCodeDir{
+        CREATED_DIRECTORY,
+        FAILED_TO_CREATE,
+        DIRECTORY_ALREADY_EXISTS
+    };  
+    Q_ENUM(ErrorCodeDir)
+
+    enum class ErrorCodePlugins{
+        PLUGIN_IS_NOT_A_BASENAVIWIDGET,
+        FAILED_TO_LOAD_PLUGIN
+    };
+    Q_ENUM(ErrorCodePlugins)
+
+    struct ErrorDataPlugins{
+        ErrorCodePlugins errorCode;
+        QString filename;
+        QString error;
+    };
+
+    using ErrorsLoad = QVector<ErrorDataPlugins>;
+
     explicit PluginLoader(QObject *parent = nullptr);
-    QVector<BaseNaviWidget*> loadPlugins(const QString &path); // Загрузка плагинов из указанной папки
+    QVector<BaseNaviWidget*> loadPlugins(ErrorsLoad &errors,const QString &path = PLAGIN_DATA_DIR); // Загрузка плагинов из указанной папки
+    ErrorCodeDir init();
 };
--- src/PluginLoader/PluginLoader.cpp
+++ src/PluginLoader/PluginLoader.cpp
@@ -3,10 +3,35 @@
 
 PluginLoader::PluginLoader(QObject *parent) : QObject(parent) {}
 
-QVector<BaseNaviWidget*> PluginLoader::loadPlugins(const QString &path) {
+PluginLoader::ErrorCodeDir PluginLoader::init(){
+    QDir dir(PLAGIN_DATA_DIR);
+
+    QMetaEnum metaEnum = QMetaEnum::fromType<ErrorCodeDir>();
+    
+    ErrorCodeDir errorCode;
+    if(!dir.exists()) {
+        if(!dir.mkpath(".")) {
+            const char* errorCodeStr = metaEnum.valueToKey(static_cast<int>(ErrorCodeDir::FAILED_TO_CREATE));
+            qWarning() << errorCodeStr << PLAGIN_DATA_DIR;
+            return ErrorCodeDir::FAILED_TO_CREATE;
+        } else {
+            errorCode = ErrorCodeDir::CREATED_DIRECTORY;
+        }
+    } else {
+        errorCode = ErrorCodeDir::DIRECTORY_ALREADY_EXISTS;
+    }
+
+    const char* errorCodeStr = metaEnum.valueToKey(static_cast<int>(errorCode));
+    qDebug() << errorCode << PLAGIN_DATA_DIR;
+    return errorCode;
+}
+
+
+QVector<BaseNaviWidget*> PluginLoader::loadPlugins(ErrorsLoad &errors,const QString &path) {
     QVector<BaseNaviWidget*> plugins;
     QDir pluginsDir(path);
-    
+    QMetaEnum metaEnum = QMetaEnum::fromType<ErrorCodePlugins>();
+
     for (const QString &fileName : pluginsDir.entryList(QDir::Files)) {
         QPluginLoader pluginLoader(pluginsDir.absoluteFilePath(fileName));
         QObject *plugin = pluginLoader.instance();
@@ -16,10 +41,14 @@ QVector<BaseNaviWidget*> PluginLoader::loadPlugins(const QString &path) {
             if (naviWidget) {
                 plugins.append(naviWidget);
             } else {
-                qWarning() << "Plugin is not a BaseNaviWidget:" << fileName;
+                const char* errorCodeStr = metaEnum.valueToKey(static_cast<int>(ErrorCodePlugins::PLUGIN_IS_NOT_A_BASENAVIWIDGET));
+                qWarning() << errorCodeStr << fileName;
+                errors.push_back({ErrorCodePlugins::PLUGIN_IS_NOT_A_BASENAVIWIDGET,fileName,""});
             }
         } else {
-            qWarning() << "Failed to load plugin:" << fileName << "Error:" << pluginLoader.errorString();
+            const char* errorCodeStr = metaEnum.valueToKey(static_cast<int>(ErrorCodePlugins::FAILED_TO_LOAD_PLUGIN));
+            qWarning() << errorCodeStr << fileName << "Error:" << pluginLoader.errorString();
+            errors.push_back({ErrorCodePlugins::PLUGIN_IS_NOT_A_BASENAVIWIDGET,fileName,pluginLoader.errorString()});
         }
     }
 
